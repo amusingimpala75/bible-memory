2b43f252a60ca10d9b4f9ebad8bdd30a9469e17d
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-03-07 10:49:19 -0500

    Add the ability to have multiple verses

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use dioxus::prelude::*;
 use reqwest;
 use regex::{Regex, Replacer};
 use std::iter::zip;
+use std::sync::LazyLock;
+use futures::future::try_join_all;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -10,15 +12,16 @@ fn main() {
     dioxus::launch(App);
 }
 
-const BOOKS: &str = include_str!("books.txt");
+const BOOKS_TEXT: &str = include_str!("books.txt");
+static BOOKS: LazyLock<Vec<String>> = LazyLock::new(|| { BOOKS_TEXT.lines().map(|s| s.to_string() ).collect() });
 
 #[component]
-fn TextInputField(id: &'static str, label: &'static str, sig: Signal<String>) -> Element {
+fn TextInputField(class: &'static str, label: &'static str, sig: Signal<String>) -> Element {
     rsx! {
         div {
             label { "{label}" }
             input {
-                id,
+                class,
                 type: "text",
                 value: "{sig}",
                 onchange: move |data| {
@@ -33,22 +36,173 @@ const DEFAULT_BOOK: &str = "John";
 const DEFAULT_VERSE: &str = "3:16";
 const DEFAULT_TEXT: &str = "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.";
 
+#[derive(Clone, PartialEq, Eq)]
+struct ReferenceData {
+    book: String,
+    reference: String,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct VerseData {
+    verses: Vec<String>,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct ConvertedVerseData {
+    converted: Vec<String>,
+}
+
+impl ReferenceData {
+    fn init() -> ReferenceData {
+        ReferenceData {
+            book: DEFAULT_BOOK.to_string(),
+            reference: DEFAULT_VERSE.to_string(),
+        }
+    }
+    async fn fetch_verses(&self, api_key: String) -> Result<VerseData> {
+        let b = self.book.as_str();
+        let r = self.reference.as_str();
+        let text = reqwest::Client::new()
+            .get(format!("https://api.esv.org/v3/passage/text/"))
+            .header("Authorization", format!("Token {api_key}"))
+            .query(&[
+                ("q", format!("{b}+{r}")),
+                ("include-passage-references", false.to_string()),
+                ("include-footnotes", false.to_string()),
+                ("include-headings", false.to_string()),
+            ])
+            .send()
+            .await?
+            .json::<BibleResponse>()
+            .await?
+            .passages
+            .join("");
+
+        let splitter = Regex::new(r"\[[0-9]*\]").expect("invalid regex");
+
+        let verses = splitter.split(&text)
+            .skip(1);               // Skip first because it is empty (i.e. before the first verse marker)
+        let numbers = splitter.find_iter(&text)
+            .map(|s| s.as_str());
+
+        let verses = zip(numbers, verses)
+            .map(|(n, v)| format!("{n} {v}"))
+            .collect();
+
+        Ok(VerseData { verses })
+    }
+}
+
+impl VerseData {
+    fn convert(&self) -> ConvertedVerseData {
+        ConvertedVerseData { converted: convert_text(&self.verses) }
+    }
+}
+
+#[component]
+fn Reference(references: Signal<Vec<ReferenceData>>, idx: usize) -> Element {
+    let books: &Vec<String> = &*BOOKS;
+    
+    rsx! {
+        if references.read().len() > 1 {
+            div {
+                button {
+                    class: "remove-reference",
+                    onclick: move |_| async move {
+                        references.write().remove(idx);
+                    },
+                    "-"
+                }
+            }
+        }
+        div {
+            label { "Book:" }
+            select {
+                class: "book",
+                onchange: move |data| {
+                    references.write()[idx].book = data.data.value()
+                },
+                for opt in books {
+                    option {
+                        selected: references.read()[idx].book == opt.clone(),
+                        value: opt.clone(),
+                        div { "{opt}" },
+                    }
+                }
+            }
+        }
+        // TODO validate
+        div {
+            label { "Chapter and Verse:" }
+            input {
+                class: "reference",
+                type: "text",
+                value: "{references.read()[idx].reference}",
+                onchange: move |data| {
+                    references.write()[idx].reference = data.data.value()
+                },
+            }
+        }
+    }
+}
+
+#[component]
+fn Verses(references: Signal<Vec<ReferenceData>>, verses: Signal<Vec<VerseData>>, idx: usize) -> Element {
+    rsx! {
+        div {
+            class: "printable",
+
+            "{references()[idx].book} {references()[idx].reference}"
+        }
+
+        div {
+            class: "printable verses",
+
+            for v in &verses()[idx].verses {
+                div { "{v}" }
+            }
+        }
+        
+    }
+}
+
+#[component]
+fn ConvertedVerses(references: Signal<Vec<ReferenceData>>, converted: Memo<Vec<ConvertedVerseData>>, idx: usize) -> Element {
+    rsx! {
+        div {
+            class: "printable",
+
+            "{references()[idx].book} {references()[idx].reference}"
+        }
+
+        div {
+            class: "printable verses",
+            
+            for v in &converted()[idx].converted {
+                div { "{v}" }
+            }
+        }
+        
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct BibleResponse {
+    passages: Vec<String>,
+}
+
 #[component]
 fn App() -> Element {
-    let book_options: Vec<_> = BOOKS.lines().map(|s| s.to_string()).collect();
-
     // TODO remember api key with cookie?
     let api_key = use_signal(|| String::new());
 
-    let mut book: Signal<String> = use_signal(|| DEFAULT_BOOK.into());
-    let reference: Signal<String> = use_signal(|| DEFAULT_VERSE.into());
-
-    let mut single_page: Signal<bool> = use_signal(|| false);
-
-    let mut retrieved_text: Signal<Vec<String>> = use_signal(|| vec! { DEFAULT_TEXT.into() });
-    let converted_text = use_memo(move || convert_text(&retrieved_text()));
+    let mut single_page = use_signal(|| false);
 
     let mut err_msg: Signal<Option<String>> = use_signal(|| None);
+
+    let mut references = use_signal(|| vec!(ReferenceData::init()));
+    let mut verses = use_signal(||vec!(VerseData { verses: vec!(DEFAULT_TEXT.to_string()) }));
+    let converted = use_memo(move || verses.read().iter().map(VerseData::convert).collect::<Vec<ConvertedVerseData>>() );
 
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
@@ -57,79 +211,83 @@ fn App() -> Element {
         div { id: "title", "Bible Memorization Helper" }
         div {
             id: "inputs",
-            // TODO validate
-            TextInputField { id: "api-key", label: "API Key:", sig: api_key.clone() }
             div {
-                label { "Book:" }
-                select {
-                    id: "book",
-                    onchange: move |data| {
-                        book.set(data.data.value());
-                    },
-                    for opt in book_options {
-                        option {
-                            selected: book() == opt.clone(),
-                            value: opt,
-                            div { "{opt}" },
+                id: "control-inputs",
+                // TODO validate
+                TextInputField { class: "api-key", label: "API Key:", sig: api_key.clone() }
+
+                div {
+                    label { "Single page?" }
+                    input {
+                        type: "checkbox",
+                        checked: single_page,
+                        onchange: move |_| {
+                            single_page.set(!single_page());
                         }
                     }
                 }
-            }
-            // TODO validate
-            TextInputField { id: "reference", label: "Chapter and Verse:", sig: reference.clone() }
-            div {
-                label { "Single page?" }
-                input {
-                    type: "checkbox",
-                    checked: single_page,
-                    onchange: move |_| {
-                        single_page.set(!single_page());
-                    }
-                }
-            }
-            div {
-                button {
-                    id: "submit",
-                    onclick: move |_| async move {
-                        let b = book();
-                        let r = reference();
-                        let gotten = get_verses(format!("{b}+{r}"), api_key()).await;
-                        if let Ok(text) = gotten { // TODO: add empty check
-                            retrieved_text.set(text);
+                div {
+                    button {
+                        id: "submit",
+                        onclick: move |_| async move {
                             err_msg.set(None);
-                        } else {
-                            retrieved_text.set(vec!());
-                            err_msg.set(Some("An error occurred processing your request".to_string())); // TODO: better messaging
-                        }
-                    },
-                    "Submit",
+                            let mapped = try_join_all(
+                                references().iter().map(|r| r.fetch_verses(api_key().to_string()))
+                            ).await;
+                            if let Ok(v) = mapped {
+                                verses.set(v);
+                            } else {
+                                err_msg.set(Some("Error occured fetching verse data".to_string()));
+                                verses.set(
+                                    vec!(VerseData {
+                                        verses: vec!("".to_string())
+                                    })
+                                    .into_iter()
+                                    .cycle()
+                                    .take(references().len())
+                                    .collect());
+                            }
+                        },
+                        "Submit",
+                    }
+                }
+                div {
+                    button {
+                        id: "add_verse",
+                        onclick: move|_| async move {
+                            references.write().push(ReferenceData::init());
+                            verses.write().push(VerseData { verses: vec!(DEFAULT_TEXT.to_string()) })
+                        },
+                        "+"
+                    }
+                }
+            }
+
+            div {
+                for idx in 0..references().len() {
+                    div {
+                        class: "reference-input",
+                        Reference { references, idx }
+                    }
                 }
             }
         }
-        div {
-            class: "printable",
 
-            "{book} {reference}"
+        div { "Original Text:" }
+        for idx in 0..references().len() {
+            Verses { references, verses, idx }
         }
-        div {
-            class: "printable verses",
 
-            "Original Text:",
-            for v in retrieved_text() {
-                div { "{v}" }
-            }
-        }
         if !single_page() {
             div { id: "print-page-break" }
         }
-        div {
-            class: "printable verses",
-            
-            "Memorization Text:",
-            for v in converted_text() {
-                div { "{v}" }
-            }
+
+        div { "Memorization Text:" }
+
+        for idx in 0..references().len() {
+            ConvertedVerses { references, converted, idx }
         }
+
         if let Some(msg) = err_msg() {
             div { "Error: {msg}" }
         }
@@ -155,38 +313,4 @@ impl Replacer for WordShortener {
 fn convert_verse_regex(str: &String) -> String {
     let word_select = Regex::new(r"([a-zA-Z]+)\s*").expect("invalid regex");
     word_select.replace_all(str, WordShortener).to_string()
-}
-
-#[derive(serde::Deserialize)]
-struct BibleResponse {
-    passages: Vec<String>,
-}
-
-async fn get_verses(reference: String, api_key: String) -> Result<Vec<String>> {
-    let text = reqwest::Client::new()
-        .get(format!("https://api.esv.org/v3/passage/text/"))
-        .header("Authorization", format!("Token {api_key}"))
-        .query(&[
-            ("q", reference),
-            ("include-passage-references", false.to_string()),
-            ("include-footnotes", false.to_string()),
-            ("include-headings", false.to_string()),
-        ])
-        .send()
-        .await?
-        .json::<BibleResponse>()
-        .await?
-        .passages
-        .join("");
-
-    let splitter = Regex::new(r"\[[0-9]*\]").expect("invalid regex");
-
-    let verses = splitter.split(&text)
-        .skip(1);               // Skip first because it is empty (i.e. before the first verse marker)
-    let numbers = splitter.find_iter(&text)
-        .map(|s| s.as_str());
-
-    Ok(zip(numbers, verses)
-        .map(|(n, v)| format!("{n} {v}"))
-        .collect())
 }
